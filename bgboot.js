0ac7fe0b616e040a8033bfc1a3eb264ec37a41a5
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2026-09-05 15:46:04 -0700

    Add VsKahlina bgboot and codeboot loaders

---
+++ bgboot.js
@@ -0,0 +1,1 @@
+window.STADIUM_BG='data:image/jpeg;base64,'+(window.__BG||[]).join('');
---
+++ codeboot.js
@@ -0,0 +1,1 @@
+(function(){var b=(window.__CODE||[]).join('');var s=atob(b);var u=new Uint8Array(s.length);for(var i=0;i<s.length;i++)u[i]=s.charCodeAt(i);(0,eval)(new TextDecoder('utf-8').decode(u));})();
