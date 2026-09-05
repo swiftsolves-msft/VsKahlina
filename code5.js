c69fc0832ff43d31825d2d0d4e0a8f72ee954da3
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2026-09-05 15:49:57 -0700

    Fix code5.js game tail chunk

--- code5.js
+++ code5.js
@@ -1,1 +1,1 @@
-window.__CODE=(window.__CODE||[]);window.__CODE.push("Iik7Cn0pKCkgOwo=");
+window.__CODE=(window.__CODE||[]);window.__CODE.push("KTsKICByZXNldEtlZXBlcigpOwogIGxvYWRCZygpLnRoZW4oKCkgPT4gewogICAgcmVxdWVzdEFuaW1hdGlvbkZyYW1lKGxvb3ApOwogIH0pOwp9KSgpOwo=");
